69e3641a52b74998034c644a633b8351af77d131
Dhindaa28 <dhindachyaa@students.undip.ac.id>
2025-11-27 22:17:52 +0700

    final code

--- vite.config.js
+++ vite.config.js
@@ -4,37 +4,54 @@ import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: false,
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: false,
 
-      pwaAssets: {
-        disabled: false,
-        config: true,
-      },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+      
+      includeAssets: ['favicon.ico', 'logo.png'], 
 
-      manifest: {
-        name: 'umkm-connect',
-        short_name: 'umkm-connect',
-        description: 'TAPraktikumPPB',
-        theme_color: '#ffffff',
-      },
+      manifest: {
+        name: 'umkm-connect',
+        short_name: 'umkm-connect',
+        description: 'TAPraktikumPPB',
+        theme_color: '#ffffff',
 
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-        cleanupOutdatedCaches: true,
-        clientsClaim: true,
-      },
+        icons: [
+          {
+            src: '/logo.png', 
+            sizes: '192x192', 
+            type: 'image/png',
+            purpose: 'any maskable', 
+          },
+          {
+            src: '/logo.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
 
-      devOptions: {
-        enabled: false,
-        navigateFallback: 'index.html',
-        suppressWarnings: true,
-        type: 'module',
-      },
-    })
-  ],
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
+    })
+  ],
 })
